3768e993f2b7d57f5148ac6adc21f5753f625f67
samteb <sam.teboul@gmail.com>
2019-12-13 16:08:47 +0200

    change app name + add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCjccpzt2b4BL0jSnDNrgGQyDlDpvZB-uQ",
+    authDomain: "fitness-manager-6a6f4.firebaseapp.com",
+    databaseURL: "https://fitness-manager-6a6f4.firebaseio.com",
+    projectId: "fitness-manager-6a6f4",
+    storageBucket: "fitness-manager-6a6f4.appspot.com",
+    messagingSenderId: "832256687093",
+    appId: "1:832256687093:web:663804c24efdc341a17ebc"
+  }
 };
